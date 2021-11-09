7e097497c0e97f9eafeaa4e8b141cd66f2adcabc
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2021-11-09 13:31:25 +0100

    Create EditUserController.php

---
+++ src/Controller/EditUserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EditUserController extends AbstractController
+{
+    public function __construct(private Security $security)
+    {
+        
+    }
+    #[Route('api/user/edit', name:'editUser',methods:['patch'])]
+    public function editUser(Request $request, UserRepository $repo, EntityManagerInterface $em)
+    {
+        $userId = $this->security->getUser()->getId();
+        $user = $repo->find($userId);
+        $data = json_decode($request->getContent(),true);
+        // dd(json_decode($request->getContent(),true)['firstname']);
+        $data['firstname'] ? $user->setFirstname($data['firstname']): null;
+        $data['lastname'] ? $user->setLastname($data['lastname']): null;
+        $data['adress'] ? $user->setAdress($data['adress']) : null;
+
+        $em->persist($user);
+        $em->flush();
+        return new Response("edited",200);
+    }
+}
